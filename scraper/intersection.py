abf2865cb92f46a4fc33ccd864dd557854ba87a1
SereiN_X <55652807+RideDi@users.noreply.github.com>
2021-04-15 22:30:51 +0800

    Update intersection.py

--- scraper/intersection.py
+++ scraper/intersection.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-df_followerList = pd.read_csv('followerList.csv',names=['userid','username','name'])
-df_followingList = pd.read_csv('followingList.csv',names=['userid','username','name'])
+df_followerList = pd.read_csv('/data/followerList.csv',names=['userid','username','name'])
+df_followingList = pd.read_csv('/data/followingList.csv',names=['userid','username','name'])
 # read two csv files
 df_followerList_followingList= pd.merge(df_followerList,df_followingList,left_on="userid",right_on = "userid", how = "inner")
 # inner join the data and index is userid
 
 df_followerList_followingList.drop(['username_y','name_y'],axis=1, inplace=True)
-df_followerList_followingList.to_csv('Result.csv',index=0)
+df_followerList_followingList.to_csv('/data/FocusOnEachOther.csv',index=0)
 # save the file and .... remoce the index
