0eda404acf483858e0e2fa53116cfaa33a4d3dbe
leigongma <liangqile@outlook.com>
2021-05-14 10:22:18 +0800

    更改了API，现在机器人预测API返回的是当前用户为机器人的概率

--- API/botPredictorAPI.py
+++ API/botPredictorAPI.py
@@ -11,13 +11,17 @@ from flask_restful import reqparse, abort, Api, Resource, request
 from Net import Net
 import json
 import pandas as pd
+from pytorch_tabnet.tab_model import TabNetClassifier
 
 app = Flask(__name__)
 api = Api(app)
 num_features = 21
-net = Net(num_features)
-net.load_state_dict(torch.load('../net_weights.pth', map_location=torch.device('cpu')))
-net.eval()
+# net = Net(num_features)
+# net.load_state_dict(torch.load('../net_weights.pth', map_location=torch.device('cpu')))
+# net.eval()
+loaded_clf = TabNetClassifier()
+saved_filepath = "../tabnet_model_test_1.zip"
+loaded_clf.load_model(saved_filepath)
 
 
 class predictResults(Resource):
@@ -25,15 +29,17 @@ class predictResults(Resource):
         data = request.get_data()
         jsonData = json.loads(data)
         df = pd.DataFrame(jsonData.items())
-        df = df.drop(axis=1,columns=0)
+        df = df.drop(axis=1, columns=0)
         df = df.transpose()
         df.astype(float)
-        xPredict = torch.from_numpy(df.to_numpy()).float()
-        result = net(xPredict)
+        # xPredict = torch.from_numpy(df.to_numpy()).float()
+        xPredict2 = df.values
+        loaded_preds = loaded_clf.predict_proba(xPredict2)
+        # result = net(xPredict)
         resultData = {}
-        resultData['result'] = torch.sigmoid(result).round().item()
-        return json.dumps(resultData)
-
+        # resultData['result'] = torch.sigmoid(result).round().item()
+        resultData['result'] = loaded_preds[0, 0]
+        return json.dumps(str(resultData))
 
 
 api.add_resource(predictResults, '/predictTasks')
